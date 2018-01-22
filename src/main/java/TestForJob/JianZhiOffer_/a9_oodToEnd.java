ed5600cd7f46c735ab69c294a6509f02bf34b51f
CygnusMoci <448111558@qq.com>
2018-01-22 14:21:42 +0800

    剑指offer

---
+++ src/main/java/TestForJob/JianZhiOffer_/a9_oodToEnd.java
@@ -0,0 +1,24 @@
+package TestForJob.JianZhiOffer_;
+
+/**
+ * @author moci
+ * @create 2018-01-19 9:48 AM
+ **/
+public class a9_oodToEnd {
+    public static void main(String[] args) {
+//        int[] array = new int[]{1,2,3,4,5,6,12,423,123,57,2312,34,2,1,4,5,1};
+//
+//        reOrderArray(array);
+//
+//        for (int i = 0; i <array.length; i++) {
+//            System.out.println(array[i]);
+//        }
+    }
+
+    public  static  void reOrderArray(int [] array) {
+        int temp = 0;
+        int len = array.length;
+    }
+
+
+}
